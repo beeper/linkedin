1cc89ceb819f795ca8729ff9e11fac98b6855d06
Sumner Evans <me@sumnerevans.com>
2021-06-01 11:45:02 -0600

    add retrieval of conversation history

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import json
 import sys
+import textwrap
 from pathlib import Path
+from datetime import datetime
 
 from linkedin_api import Linkedin
 from requests.cookies import cookiejar_from_dict
+
+cookies = cookiejar_from_dict(
+    {
+        "liap": "true",
+        "li_at": sys.argv[1],
+        "JSESSIONID": sys.argv[2],
+    }
+)
+linkedin = Linkedin("", "", cookies=cookies)
 
 convo_cache_path = Path("convocache.json")
 if convo_cache_path.exists():
@@ -11,19 +22,17 @@ if convo_cache_path.exists():
     with open(convo_cache_path) as f:
         convos = json.load(f)
 else:
-    cookies = cookiejar_from_dict(
-        {
-            "liap": "true",
-            "li_at": sys.argv[1],
-            "JSESSIONID": sys.argv[2],
-        }
-    )
-    linkedin = Linkedin("", "", cookies=cookies)
-
     convos = linkedin.get_conversations()
 
-    with open("convocache.json", "w+") as convocache:
+    with open(convo_cache_path, "w+") as convocache:
         json.dump(convos, convocache, sort_keys=True, indent=2)
+
+thread_cache_path = Path("threadcache.json")
+threads = {}
+if thread_cache_path.exists():
+    print("Using threadcache.json")
+    with open(thread_cache_path) as f:
+        threads = json.load(f)
 
 
 def get_path(obj, *path):
@@ -53,3 +62,42 @@ for convo in convos["elements"]:
                         pictureArtifacts[0].get("fileIdentifyingUrlPathSegment"),
                     )
                 )
+
+    print("Thread")
+    urn = convo["entityUrn"].split(":")[-1]
+
+    if urn in threads:
+        thread = threads[urn]
+    else:
+        thread = linkedin.get_conversation(urn)
+        threads[urn] = thread
+
+    for element in thread.get("elements", []):
+        time = datetime.utcfromtimestamp(element.get("createdAt") // 1000)
+
+        sender_profile = get_path(
+            element,
+            "from",
+            "com.linkedin.voyager.messaging.MessagingMember",
+            "miniProfile",
+        )
+        sender = "{} {}".format(
+            sender_profile.get("firstName"), sender_profile.get("lastName")
+        )
+        print(f"  {sender} @ {time}")
+        print(f"    Reactions: {element['reactionSummaries']}")
+        paragraphs = get_path(
+            element,
+            "eventContent",
+            "com.linkedin.voyager.messaging.event.MessageEvent",
+            "attributedBody",
+            "text",
+        ).split("\n")
+        for text in paragraphs:
+            wrapped = textwrap.wrap(text, 80)
+            for line in wrapped:
+                print(f"    {line}")
+            print()
+
+with open(thread_cache_path, "w+") as f:
+    json.dump(threads, f, sort_keys=True, indent=2)
