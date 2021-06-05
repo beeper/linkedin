910093e029665f8152d6c05e50a93acbfc4e672b
Sumner Evans <me@sumnerevans.com>
2021-06-05 08:14:32 -0600

    ci: setting up github workflow

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import setuptools
 
-from linkedin_matrix.get_version import git_tag, git_revision, version, linkified_version
+from linkedin_matrix.get_version import (
+    git_revision,
+    git_tag,
+    linkified_version,
+    version,
+)
 
 try:
     long_desc = open("README.md").read()
@@ -24,32 +29,29 @@ with open("optional-requirements.txt") as reqs:
 extras_require["all"] = list({dep for deps in extras_require.values() for dep in deps})
 
 with open("linkedin_matrix/version.py", "w") as version_file:
-    version_file.write(f"""# Generated in setup.py
+    version_file.write(
+        f"""# Generated in setup.py
 
 git_tag = {git_tag!r}
 git_revision = {git_revision!r}
 version = {version!r}
 linkified_version = {linkified_version!r}
-""")
+"""
+    )
 
 setuptools.setup(
     name="linkedin-matrix",
     version=version,
     url="https://github.com/sumnerevans/linkedin-matrix",
-
     author="Sumner Evans",
     author_email="inquiries@sumnerevans.com",
-
     description="A Matrix-LinkedIn Messages puppeting bridge.",
     long_description=long_desc,
     long_description_content_type="text/markdown",
-
     packages=setuptools.find_packages(),
-
     install_requires=install_requires,
     extras_require=extras_require,
     python_requires="~=3.7",
-
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Communications :: Chat",
@@ -61,4 +63,3 @@ setuptools.setup(
         (".", ["linkedin_matrix/example-config.yaml"]),
     ],
 )
-
