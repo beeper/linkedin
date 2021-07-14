45bdee557debdf35c66dba54f67873eabd05b304
Sumner Evans <me@sumnerevans.com>
2021-07-14 15:57:08 -0600

    ci: fix docker tag pushing

--- cicd/docker-publish.sh
+++ cicd/docker-publish.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
+set -xe
 
 docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA-amd64
 
-if [[ "$CI_COMMIT_TAG" =~ /^v.*/ ]]; then
+if [[ $CI_COMMIT_TAG =~ ^v.*$ ]]; then
     docker tag $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA-amd64 $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG-amd64
     docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG-amd64
 fi
