957b633fbdf7e5f495fe82dc94407a76b30fb9bc
Sumner Evans <me@sumnerevans.com>
2021-08-03 15:04:16 -0600

    ci: fix docker image cache

--- cicd/docker-publish.sh
+++ cicd/docker-publish.sh
@@ -6,4 +6,6 @@ docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA-amd64
 if [[ $CI_COMMIT_TAG =~ ^v.*$ ]]; then
     docker tag $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA-amd64 $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG-amd64
     docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG-amd64
+    docker tag $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA-amd64 $CI_REGISTRY_IMAGE:latest-amd64
+    docker push $CI_REGISTRY_IMAGE:latest-amd64
 fi
