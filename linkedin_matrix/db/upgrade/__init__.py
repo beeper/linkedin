9196221c32179a26ae4918d102682537438caffa
Sumner Evans <me@sumnerevans.com>
2021-08-07 23:58:22 -0600

    db upgrade: fix imports

--- linkedin_matrix/db/upgrade/__init__.py
+++ linkedin_matrix/db/upgrade/__init__.py
@@ -2,6 +2,14 @@ from mautrix.util.async_db import UpgradeTable
 
 upgrade_table = UpgradeTable()
 
-from . import v01_initial_revision, v02_multiple_reaction_per_message  # noqa: E402
+from . import (  # noqa: E402
+    v01_initial_revision,
+    v02_multiple_reaction_per_message,
+    v03_add_topic_to_portal,
+)
 
-__all__ = ("v01_initial_revision", "v02_multiple_reaction_per_message")
+__all__ = (
+    "v01_initial_revision",
+    "v02_multiple_reaction_per_message",
+    "v03_add_topic_to_portal",
+)
