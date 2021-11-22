49e6cea03576318ed29cf3335beab547f1d32aa3
Sumner Evans <me@sumnerevans.com>
2021-11-22 14:31:01 -0700

    real_user_content_key: change to com.beeper.linkedin.puppet

--- linkedin_matrix/__main__.py
+++ linkedin_matrix/__main__.py
@@ -25,7 +25,7 @@ class LinkedInBridge(Bridge):
     command = "linkedin-matrix"
     description = "A Matrix-LinkedIn Messages puppeting bridge."
     repo_url = "https://gitlab.com/beeper/linkedin"
-    real_user_content_key = "com.sumnerevans.linkedin.puppet"
+    real_user_content_key = "com.beeper.linkedin.puppet"
     version = version
     markdown_version = linkified_version
     config_class = Config
